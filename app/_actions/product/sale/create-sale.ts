7e471fc5f41875d56183dc6fd4fb385abb93a97d
zFelipez <felipefesilva19@gmail.com>
2026-06-11 14:36:36 -0300

    feat: ensure sale creation is done with a transaction

--- app/_actions/product/sale/create-sale.ts
+++ app/_actions/product/sale/create-sale.ts
@@ -7,55 +7,52 @@ import { revalidatePath } from "next/cache";
 export const createSale = async (products: CreateSaleSchema) => {
   createSaleSchema.parse(products);
 
-  const sale = await db.sale.create({
-    data: {
-      date: new Date(), // crio uma nova venda
-    },
-  });
-
-  for (const product of products.products) {
-    const productFromDb = await db.product.findUnique({
-      where: {
-        id: product.id,
+  await db.$transaction(async (tsx) => { //garantir que todas as operações sejam atômicas, ou seja, ou todas são bem-sucedidas ou nenhuma é aplicada
+    const sale = await tsx.sale.create({
+      data: {
+        date: new Date(), // crio uma nova venda
       },
     });
 
-    if (!productFromDb) {
-      throw new Error("Unique price is not defined");
-    }
-
-    const productIsOutOfStock = productFromDb.stock < 0;
-
-    if (productIsOutOfStock) {
-      throw new Error("Product is out of stock ");
-    }
-
-    await db.saleProduct.create({
-      data: {
-        saleId: sale.id,
-        productId: product.id,
-        quantity: product.quantity,
-        unitPrice: productFromDb.price as Decimal,
-      },
-    });
-
-
-    await db.product.update({
-      where: {
-        id: product.id,
-      },
-      data: {
-        stock: {
-          decrement: product.quantity,
+    for (const product of products.products) {
+      const productFromDb = await db.product.findUnique({
+        where: {
+          id: product.id,
         },
-      },
-    });
+      });
 
-  revalidatePath("/sales");
-  revalidatePath("/products");
- 
-  }
+      if (!productFromDb) {
+        throw new Error("Unique price is not defined");
+      }
 
+      const productIsOutOfStock = productFromDb.stock < 0;
 
-  
+      if (productIsOutOfStock) {
+        throw new Error("Product is out of stock ");
+      }
+
+      await tsx.saleProduct.create({
+        data: {
+          saleId: sale.id,
+          productId: product.id,
+          quantity: product.quantity,
+          unitPrice: productFromDb.price as Decimal,
+        },
+      });
+
+      await tsx.product.update({
+        where: {
+          id: product.id,
+        },
+        data: {
+          stock: {
+            decrement: product.quantity,
+          },
+        },
+      });
+
+      revalidatePath("/sales");
+      revalidatePath("/products");
+    }
+  });
 };
