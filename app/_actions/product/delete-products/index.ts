2d51eee91ca8a1b68b65169b71e84adbc0309f1c
zFelipez <felipefesilva19@gmail.com>
2026-06-16 21:08:50 -0300

    fix:ensure  product stock is restored after slae is deleted

--- app/_actions/product/delete-products/index.ts
+++ app/_actions/product/delete-products/index.ts
@@ -5,13 +5,45 @@ import { deleteIdSchema } from "./schema";
 import { revalidatePath } from "next/cache";
 import { actionClient } from "@/app/_lib/safe-action";
 
-export const deleteProductAction = actionClient.schema(deleteIdSchema).action(
-  async ({ parsedInput: { id } }) => {
-    await db.product.delete({
-      where: {
-        id,
-      },
+export const deleteProductAction = actionClient
+  .schema(deleteIdSchema)
+  .action(async ({ parsedInput: { id } }) => {
+    await db.$transaction(async (trx) => {
+      const sale = await trx.sale.findUnique({
+        where: {
+          id: id,
+        },
+
+        include: {
+          products: true,
+        },
+      });
+
+      if (!sale) {
+        return;
+      }
+
+      await trx.product.delete({
+        where: {
+          id,
+        },
+      });
+
+      for (const product of sale.products) {
+        await trx.product.update({
+          where: {
+            id: product.productId,
+          },
+          data: {
+            stock: {
+              increment: product.quantity,
+            },
+          },
+        });
+      }
     });
+
     revalidatePath("/products");
-  },
-);
+    revalidatePath("/");
+    revalidatePath("/sales");
+  });
