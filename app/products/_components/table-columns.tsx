7cee2497101e40e8b766797e275d93a0d458621f
zFelipez <felipefesilva19@gmail.com>
2026-06-20 00:38:48 -0300

    fix: add actions column to products table for improved functionality

--- app/products/_components/table-columns.tsx
+++ app/products/_components/table-columns.tsx
@@ -4,6 +4,7 @@ import { Product } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 
 import { StatusBadge } from "@/app/_components/status-badge";
+import { ActionsCell } from "./actions-cell";
 
 export const productsTableColumns: ColumnDef<Product>[] = [
   {
@@ -37,4 +38,12 @@ export const productsTableColumns: ColumnDef<Product>[] = [
       );
     },
   },
+  {
+    accessorKey: "actions",
+    header: "Ações",
+    cell: (row) => {
+      const product = row.row.original;
+      return <ActionsCell id={product.id} name={product.name} price={product.price} stock={product.stock}></ActionsCell>;
+    },
+  }
 ];
